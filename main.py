3f2e3694aea1b8a8c37a982d0153891b57819fbc
ShellYu <yuysyu@gmail.com>
2013-09-11 12:51:49 -0400

    main.py is the main file of this project

---
+++ main.py
@@ -0,0 +1,9 @@
+# this is a app which can pose the image of detroit from 
+# instagram to other website.
+
+# http://instagram.com/developer/endpoints/tags/#get_tags_search
+
+taglist = "https://api.instagram.com/v1/tags"
+"/snow/media/recent?access_token=229779614.f59def8.6d5e18dfd4e94210838c01537de99a50"
+tagstr = "instadetroit"
+access_token = ""  
